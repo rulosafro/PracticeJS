0a8f2ff9eca067f6798ba7af3cb2e71b94365159
Javier Ramírez <jramirez2432@gmail.com>
2022-11-07 23:26:25 -0300

    rap5

--- js/dist/entrega2.dev.js
+++ js/dist/entrega2.dev.js
@@ -1,38 +1,112 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/* ! Cosas por hacer: Branch con los comentarios */
+// Variables
+var nombre;
+var genM;
+var carroDeCompra = [];
+/*
+function nombrePersona () {
+    let nombre = prompt(`¿Cual es su nombre?`)
+    alert(nombre + ` un gusto verte por aquí. Veamos que estas buscando en GuitarWorld`)
+}
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+const genMusic = () => {
+    let genM = prompt(`¿Cual es tu genero musical favorito? \n Rock \n Jazz \n Rap \n Blues`)
+    return(genM)
+}
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-/* ! 
-Cosas por hacer 
-- Branch con los comentarios
-- Base de datos 
-- Variables, Obtos JS
-- Funcionalidad inicial
-- Flujo de trabajo ene l script capturando las entradas ingresadas por el usuario prompt() y salida por medio de alert()
+// Desarrollo
+alert(`Bienvenido a GuitarWorld, acá podras comprar todo lo necesario para tu viaje musical`)
+nombrePersona()
+while (genM != "rock" && genM != "jazz" && genM != "rap" && genM != "blues") {
+    genM = prompt("Elige uno dentro de las opciones \n Rock \n Jazz \n Rap \n Blues").toLowerCase()
+    switch (genM) {
+        case "rock":
+            alert(`Alto fanatico de ${genM} veamos que hay en stock que te pueda servir`)
+            break;
+        case "jazz":
+            alert(`Alto fanatico de ${genM} veamos que hay en stock que te pueda servir`)
+            break;
+        case "rap":
+            alert(`Alto fanatico de ${genM} veamos que hay en stock que te pueda servir`)
+            break;
+        case "blues":
+            alert(`Alto fanatico de ${genM} veamos que hay en stock que te pueda servir`)
+            break;
+        default:
+            alert(`Parece que no conozco todavia este genero.`)
+            console.log(`Ingreso un valor incorrecto`);
+            break;
+    }
+}
 */
-// * Objeto o Class para la persona
-var Persona =
-/*#__PURE__*/
-function () {
-  function Persona(nombre, peso, sexo, anio) {
-    _classCallCheck(this, Persona);
 
-    this.nombre = nombre;
-    this.peso = peso;
-    this.sexo = sexo;
-    this.anio = anio;
+var printStock = function printStock() {
+  var stock1 = "";
+
+  for (var i = 0; i < dataGuitarras.length; i++) {
+    stock1 += "".concat(i + 1, " - ").concat(dataGuitarras[i].marca, " ").concat(dataGuitarras[i].nombre, "- $").concat(dataGuitarras[i].precio, " \n");
   }
 
-  _createClass(Persona, [{
-    key: "mostrar",
-    value: function mostrar() {
-      console.log(this.Persona);
-    }
-  }]);
+  return stock1;
+};
 
-  return Persona;
-}();
+var elegir = prompt("Acá esta el stock actual, elige un numero de la lista \n" + printStock());
+
+var verificadorDeProducto = function verificadorDeProducto() {
+  while (isNaN(elegir)) {
+    alert("Por favor ingresa un numero");
+    elegir = prompt(printStock());
+  }
+
+  while (elegir == "") {
+    alert("Lo Sentimos. No se permiten campos vacios. Por favor ingrese un numero");
+    elegir = prompt(printStock());
+  }
+};
+
+verificadorDeProducto();
+carroDeCompra.push(dataGuitarras[elegir - 1].marca + " " + dataGuitarras[elegir - 1].nombre + " " + dataGuitarras[elegir - 1].precio);
+console.log(carroDeCompra);
+alert(carroDeCompra); // alert(`En el carro actualmente hay ` + carroDeCompra);
+// for (let i = 0; i < carroDeCompra.length; i++) {
+//     const plataTotal = carroDeCompra.precio.reduce();
+// }
+
+var iteracion;
+
+while (iteracion != "si" || iteracion != "no") {
+  var _iteracion = prompt("Quieres agregar otra compra al carro \n si \n no").toLowerCase();
+
+  switch (_iteracion) {
+    case "si":
+      var _elegir = prompt("Acá esta el stock actual, elige un numero de la lista \n" + printStock());
+
+      verificadorDeProducto(); // alert(elegir)
+
+      carroDeCompra.push(dataGuitarras[_elegir - 1].marca + " " + dataGuitarras[_elegir - 1].nombre + " " + dataGuitarras[_elegir - 1].precio);
+      console.log(carroDeCompra);
+      break;
+
+    case "no":
+      console.log("salida de iteracion");
+      alert("Okey, continuemos");
+      alert("En el carro actualemente hay \n" + plataTotal);
+      break;
+
+    default:
+      alert("No te he entendido, me puedes repetir");
+      console.log("Ingreso un valor incorrecto");
+      break;
+  }
+}
+
+alert("Okey, continuemos"); // for (let i = 0; i < carroDeCompra.length; i++) {
+//     const plataTotal = carroDeCompra.precio.reduce();
+//     return plataTotal
+// }
+// for (let i = 0; i < carroDeCompra.length; i++) {
+//     const plataTotal = carroDeCompra.precio.reduce();
+// }
+// console.log(plataTotal);
